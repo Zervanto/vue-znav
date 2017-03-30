8b3446496437d8d74d26b364dcad2ebcc4d8798f
Zervanto <1019335509@qq.com>
2017-03-30 21:14:37 +0800

    使用过滤器与计算属性

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,28 @@
 <template>
 <div>
     <p>this is login page</p>
+    <input type="text" v-model="name"> 
+    <hr>
+    <div>{{name | strToUpperCase | sliceName(6)}}</div>
 </div>
 </template>
 <script>
     export default{
         data(){
             return {
-                
+                name:''
+            }
+        },
+        filters:{
+            strToUpperCase(input){//过滤器是一个方法
+                return input.toUpperCase();                    
+            },
+            sliceName(name,length){
+            // if(length>5){
+            //        return 
+            //    }
+                return name.length>5?name.slice(0,length)+'...':name.slice(0,length);
+                // return name.slice(0,5)+'...'
             }
         }
     }
